60450c6040e9a7112110de57c2cf60e584fac2b7
Shumo Chu <stechu@users.noreply.github.com>
2023-06-08 12:47:12 -0400

    Update page.js
    Signed-off-by: Shumo Chu <stechu@users.noreply.github.com>

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             starting from Ethereum. We think zero-knowledge proof + blockchain brings better and more trustless coordination 
             to human society. And one of the remaining big problems is how to make ZKP scalable and composable on blockchains. 
             We are building the first proof aggregation service on Ethereum which uses our 
-              <strong>Universal ZK-ZK Proof Aggregation</strong> construction to aggregate all kinds of ZKPs, 
+              <strong> Universal ZK-ZK Proof Aggregation</strong> construction to aggregate all kinds of ZKPs, 
             including but not limited to as privacy preserving identities, storage proofs, ZKML, zkEVM proofs. 
             This proof aggregation service could:
             <ol>
