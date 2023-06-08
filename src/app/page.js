037067d848b06e11fa94bda4802021fb3db160a1
亮程 <cl510804194@gmail.com>
2023-06-09 01:02:20 +0800

    merge:app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,22 +10,31 @@ export default function Home() {
         </h1>
         <div className="description">
           <p>
-            We are <strong>Zug</strong> (can be also called Zug ZK), an research 
-            and community driven project. Our vision is to scale and compose zero-knowledge proof (ZKP) on blockchains,
-            starting from Ethereum. We think zero-knowledge proofs + blockchains brings better and more trustless coordination 
-            to human society. And one of the remaining big problems is how to make ZKP scalable and composable on blockchains. 
-            We are building the first proof aggregation service on Ethereum, which uses 
-            our <strong> Universal ZK-ZK Proof Aggregation</strong> construction to aggregate all kinds of ZKPs, 
-            including but not limited to as privacy preserving identities, storage proofs, ZKML, zkEVM proofs. 
-            This proof aggregation service could:
+            We are <strong>Zug</strong> (can be also called Zug ZK), an research
+            and community driven project. Our vision is to scale and compose
+            zero-knowledge proof (ZKP) on blockchains, starting from Ethereum.
+            We think zero-knowledge proofs + blockchains brings better and more
+            trustless coordination to human society. And one of the remaining
+            big problems is how to make ZKP scalable and composable on
+            blockchains. We are building the first proof aggregation service on
+            Ethereum, which uses our{' '}
+            <strong> Universal ZK-ZK Proof Aggregation</strong> construction to
+            aggregate all kinds of ZKPs, including but not limited to as privacy
+            preserving identities, storage proofs, ZKML, zkEVM proofs. This
+            proof aggregation service could:
             <ol>
-              <li>Multiply the bandwith (by orders of magnitude), and thus lower the cost of ZKP verfication on Ethereum (and its L2s)</li>
+              <li>
+                Multiply the bandwith (by orders of magnitude), and thus lower
+                the cost of ZKP verfication on Ethereum (and its L2s)
+              </li>
               <li>Composing ZKP proofs from different parties </li>
-          </ol>  
+            </ol>
           </p>
           <p>
-            If you’re excited by advanced cryptography, distributed systems, protocol design or public goods, please reach
-            out by twitter DM: we are excited to hear from you, regardless of education, experiences or culture.
+            If you’re excited by advanced cryptography, distributed systems,
+            protocol design or public goods, please reach out by twitter DM: we
+            are excited to hear from you, regardless of education, experiences
+            or culture.
           </p>
         </div>
       </div>
